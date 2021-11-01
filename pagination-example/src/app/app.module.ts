aa2f2b5c02c4ad5dd4e301a515481173890f77fe
umarshaikh <umarshaikh8452@gmail.com>
2021-11-01 16:59:40 +0530

    initial commit

---
+++ pagination-example/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxPaginationModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
